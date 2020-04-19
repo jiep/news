eb8e65d0ee0388607e3de1cfd88b3ee8a1420f96
José Ignacio Escribano <jiep12345@hotmail.com>
2020-04-19 13:50:50 -0400

    Remove unused CSS flags

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,5 @@
+const { ALLOWED_COUNTRIES } = require('./src-functions/allowed_countries.js');
+
 module.exports = {
   plugins: [
     require("tailwindcss"),
@@ -6,8 +8,7 @@ module.exports = {
       require("@fullhuman/postcss-purgecss")({
         content: ["./src/**/*.vue", "./public/index.html"],
         defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-        whitelist: ["spinner"],
-        whitelistPatterns: [/flag-icon*/]
+        whitelist: ["spinner", "flag-icon", ...ALLOWED_COUNTRIES.map(x => `flag-icon-${x}`)]
       })
   ]
 };
--- src-functions/countries.js
+++ src-functions/countries.js
@@ -1,4 +1,5 @@
-const ALLOWED_COUNTRIES = "ae ar at au be bg br ca ch cn co cu cz de eg fr gb gr hk hu id ie il in it jp kr lt lv ma mx my ng nl no nz ph pl pt ro rs ru sa se sg si sk th tr tw ua us ve za".split(" ");
+const { ALLOWED_COUNTRIES } = require("./allowed_countries.js");
+console.log(ALLOWED_COUNTRIES);
 
 exports.handler = async (event, context) => {
 
