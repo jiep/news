c7bc388c0278230740ca1826ae4472a9710b1c48
José Ignacio Escribano <jiep12345@hotmail.com>
2020-04-19 15:42:12 -0400

    New version 0.3.0

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -2,7 +2,7 @@
 .bg-gray-200.w-full.flex-grow.overflow-y-auto
   template(v-if="news.length > 0")
     .flex.justify-start.content-center.flex-col.p-4(v-for="news in news")
-      news.shadow-lg(:news="news", class='hover:bg-gray-300')
+      news.shadow-lg.rounded-lg.overflow-hidden(:news="news", class='hover:bg-gray-300')
   template(v-else)
     .flex.items-center.justify-center.h-full
       p.text.text-gray-700 No news found!
