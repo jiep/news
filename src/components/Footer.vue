d61a11ba0c9191a1437e47a0f4888ade3cc15da3
José Ignacio Escribano <jiep12345@hotmail.com>
2020-04-23 14:18:33 -0400

    Fix error

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,11 @@
 <template lang="pug">
-footer.bg-indigo-900.text-white.w-full.py-4.text-center.flex-grow-0.h-24
-  p.font-light Made with
-    |
-    span.text-red-600.mx-2.text-xl ♥
-    |  by 
-    a.font-hairline.text-white(:href="github", target="_blank", class="hover:underline") {{author}}
+footer.px-8
+  .w-full.py-4.text-center.flex-grow-0.h-24.border-t.border-gray-400
+    p.font-medium.text-gray-600 Made with
+      |
+      span.text-red-600.mx-2.text-xl ♥
+      |  by 
+      a.font-bold.text-gray-600(:href="github", target="_blank", class="hover:underline") {{author}}
 </template>
 
 <script>
